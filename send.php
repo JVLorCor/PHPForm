f736128a77b4e8c0a489d4902e07bcbdc128fe51
José Vladimir <jvlorcor.appventurers.com>
2024-02-26 10:58:54 +0100

    second commit

--- send.php
+++ send.php
@@ -15,15 +15,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "<p>$error</p>";
         }
         echo "</div>";
-    } else {
-        echo "<h2>Datos ingresados:</h2>";
-        echo "<p>Nombre: $nombre</p>";
-        echo "<p>Apellido: $apellido</p>";
-        echo "<p>Edad: $edad</p>";
-        echo "<p>Sexo: $sexo</p>";
-        echo "<p>Correo electrónico: $correo</p>";
-        echo "<p>Número de teléfono: $telefono</p>";
     }
+
+    echo '<a href="./">Volver</a> <br>';
 }
 
-echo '<a href="./">Volver</a> <br>';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos Formulario</title>
+    <link rel="stylesheet" href="./style/reset.css">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+<h2>Datos ingresados:</h2>
+<p>Nombre: <?php echo $nombre; ?></p>
+<p>Apellido: <?php echo $apellido; ?></p>
+<p>Edad: <?php echo $edad; ?></p>
+<p>Sexo: <?php echo $sexo; ?></p>
+<p>Correo electrónico: <?php echo $correo; ?></p>
+<p>Número de teléfono: <?php echo $telefono; ?></p>
+</body>
+</html>
