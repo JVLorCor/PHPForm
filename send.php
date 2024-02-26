f04974d5a677c62faecfc8c322d05485a598ed53
Jose V. <jvlorcor.appventurers@gmail.com>
2024-02-26 18:19:59 +0100

    button fix

--- send.php
+++ send.php
@@ -14,28 +14,54 @@ echo '<a href="./">Volver</a> <br>';
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Datos Formulario</title>
+    <link rel="stylesheet" href="./style/svg.css">
     <link rel="stylesheet" href="./style/reset.css">
     <style>
-        body {
+        .main-container {
             display: flex;
+            position: absolute;
             flex-direction: column;
-            justify-content: center;
             align-items: center;
+            justify-content: center;
             height: 100vh;
+            z-index: 10;
+            width: 100%;
+            top: 0;
+        }
+
+        .main-container a {
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .main-container a:hover {
+            background-color: #0056b3;
         }
     </style>
 </head>
+
 <body>
-<h2>Datos ingresados:</h2>
-<p>Nombre: <?php echo $nombre; ?></p>
-<p>Apellido: <?php echo $apellido; ?></p>
-<p>Edad: <?php echo $edad; ?></p>
-<p>Sexo: <?php echo $sexo; ?></p>
-<p>Correo electrónico: <?php echo $correo; ?></p>
-<p>Número de teléfono: <?php echo $telefono; ?></p>
+    <svg id="svg"></svg>
+    <div class="main-container">
+        <h2>Datos ingresados:</h2>
+        <p>Nombre: <?php echo $nombre; ?></p>
+        <p>Apellido: <?php echo $apellido; ?></p>
+        <p class="genero">Edad: <?php echo $edad; ?></p>
+        <p>Sexo: <?php echo $sexo; ?></p>
+        <p>Correo electrónico: <?php echo $correo; ?></p>
+        <p>Número de teléfono: <?php echo $telefono; ?></p>
+        <a href="./index.html">Volver</a>
+    </div>
+    <script src="./script/script.js"></script>
 </body>
+
 </html>
